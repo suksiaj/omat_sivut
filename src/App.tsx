1f383a1838c9d8d06372ce0602feee15751f8085
Jesse Suksia <djesu004@edu.xamk.fi>
2024-08-08 16:32:29 +0300

    Ensimmäiset kikkailut

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,15 @@ import { Typography } from '@mui/material';
 const App : React.FC = () : React.ReactElement => {
   return (
     <div className="App">
-      <Typography variant='h1'>Jesse Suksia</Typography>
+      <div className="App-header">
+
+        <Typography variant='h1'>Jesse Suksia</Typography>
+        <Typography variant='h2'>Data Processing Student & Security Professional</Typography>
+        <Typography variant='h5'>Software | Cybersecurity | Gaming | Physical Security</Typography>
+
+
+      </div>
+      
     </div>
   );
 }
