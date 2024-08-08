35db7539e74c041e3042a43e25ca99d4704693ad
Jesse Suksia <djesu004@edu.xamk.fi>
2024-08-08 14:50:45 +0300

    Aloitus

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Typography } from '@mui/material';
 
-function App() {
+const App : React.FC = () : React.ReactElement => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Typography variant='h1'>Jesse Suksia</Typography>
     </div>
   );
 }
