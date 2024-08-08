42d6cd6382cbec83dcac97afc42b2cb7d4c3ceaf
Jesse Suksia <djesu004@edu.xamk.fi>
2024-08-08 22:03:29 +0300

    Linkit ja muotoilua

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Button, Grid, Link, Typography } from '@mui/material';
 
-const App : React.FC = () : React.ReactElement => {
+const App: React.FC = (): React.ReactElement => {
   return (
     <div className="App">
 
       <div className="App-header">
         <div className="App-title">
-        <Typography variant='h1'>Jesse Suksia</Typography>
-        <Typography variant='h2'>Data Processing Student & Security Professional</Typography>
+          <Typography variant='h1'>Jesse Suksia</Typography>
+          <Typography variant='h3'>Data Processing Student & Security Professional</Typography>
         </div>
 
         <div className="App-link">
@@ -18,37 +17,30 @@ const App : React.FC = () : React.ReactElement => {
             container
             direction='row'
             alignItems={'flex-end'}
+            margin={5}
             justifyContent={'space-around'}>
-              <Link href='#' underline='hover' color='inherit'>
-              <Typography variant='h6'>Software</Typography>
-              </Link>
-               |
-               <Link href='#' underline='hover' color='inherit'>
-              <Typography variant='h6'>Cybersecurity</Typography>
-              </Link>
-               |
-               <Link href='#' underline='hover' color='inherit'>
-              <Typography variant='h6'>Games</Typography>
-              </Link>
-               |
-               <Link href='#' underline='hover' color='inherit'>
-              <Typography variant='h6'>Safety & Security</Typography>
-              </Link>
-               
-            </Grid>
-            
+            <Link href='/home' underline='hover' color='inherit'>
+              <Typography variant='h5'>Software</Typography>
+            </Link>
+            |
+            <Link href='/cyber' underline='hover' color='inherit'>
+              <Typography variant='h5'>Cybersecurity</Typography>
+            </Link>
+            |
+            <Link href='/games' underline='hover' color='inherit'>
+              <Typography variant='h5'>Games</Typography>
+            </Link>
+            |
+            <Link href='/sec' underline='hover' color='inherit'>
+              <Typography variant='h5'>Safety & Security</Typography>
+            </Link>
+
+          </Grid>
+
         </div>
-        
+
       </div>
-      
-      <div className="App-body">
-        <Typography variant='h3'>About Me</Typography>
-        <Typography variant='body1'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-          Amet beatae porro quas repellat laboriosam tempora voluptates optio magni minima delectus dolores, blanditiis, sed, 
-          assumenda quae quis veritatis! Pariatur, obcaecati at!
-        </Typography>
-      </div>
+
     </div>
   );
 }
