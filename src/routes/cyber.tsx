7fbb3767e11fb9ebf3d998c52bf3fb19423f68ba
Jesse Suksia <djesu004@edu.xamk.fi>
2025-02-21 20:13:35 +0200

    Muotoilua

--- src/routes/cyber.tsx
+++ src/routes/cyber.tsx
@@ -1,21 +1,38 @@
-import { Container, Paper, Typography } from '@mui/material';
+import { Container, Grid, Paper, Typography } from '@mui/material';
 import '../App.css';
 
 const Cyber: React.FC = (): React.ReactElement => {
     return (
-        <Container >
-
+        <div className='Page'>
             <div className="Page-title">
                 <Typography variant="h1">Cyber</Typography>
             </div>
 
-            <div className="Page-content">
-                <Paper elevation={24} variant='outlined' sx={{ marginTop: '10px', width: "80%", textAlign: "center", padding: "20px" }}>
-                    <iframe title="TryHackMe Profile" src="https://tryhackme.com/api/v2/badges/public-profile?userPublicId=2066148" style={{ border: 'none', width: '358px' }}></iframe>
-                </Paper>
+            <Grid container direction='row' justifyContent='center' alignItems='center'>
+                <div className="Page-content">
+                    <Paper
+                        elevation={24}
+                        variant='elevation'
+                        sx={{ marginTop: '10px', width: "80%", textAlign: "center", padding: "20px", backgroundColor: "inherit" }}>
+                        <Typography>
+                            Asiaa
+                        </Typography>
+                    </Paper>
 
-            </div>
-        </Container>
+                </div>
+                <div className="Page-content">
+                    <Paper
+                        elevation={24}
+                        variant='elevation'
+                        sx={{ marginTop: '10px', width: "80%", textAlign: "center", padding: "20px", backgroundColor: "inherit" }}>
+                        <iframe title="TryHackMe Profile" src="https://tryhackme.com/api/v2/badges/public-profile?userPublicId=2066148" style={{ border: 'none', width: '358px' }}></iframe>
+                    </Paper>
+
+                </div>
+
+            </Grid>
+
+        </div>
 
     )
 }
--- src/routes/games.tsx
+++ src/routes/games.tsx
@@ -1,11 +1,40 @@
-import { Typography } from '@mui/material';
+import { Grid, Paper, Typography } from '@mui/material';
 import '../App.css';
 
 const Games: React.FC = (): React.ReactElement => {
     return(
-        <>
-        <Typography variant="h1">Games</Typography>
-        </>
+        <div className='Page'>
+        <div className="Page-title">
+            <Typography variant="h1">Games</Typography>
+        </div>
+
+        <Grid container direction='row' justifyContent='center' alignItems='center'>
+            <div className="Page-content">
+                <Paper
+                    elevation={24}
+                    variant='elevation'
+                    sx={{ marginTop: '10px', width: "80%", textAlign: "center", padding: "20px", backgroundColor: "inherit" }}>
+                    <Typography>
+                        Asiaa
+                    </Typography>
+                </Paper>
+
+            </div>
+            <div className="Page-content">
+                <Paper
+                    elevation={24}
+                    variant='elevation'
+                    sx={{ marginTop: '10px', width: "80%", textAlign: "center", padding: "20px", backgroundColor: "inherit" }}>
+                    <Typography>
+                        Lisää asiaa
+                    </Typography>
+                </Paper>
+
+            </div>
+
+        </Grid>
+
+    </div>
     )
 }
 
