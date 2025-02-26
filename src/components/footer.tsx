0dea9a0068e10d93d7ac154e38362ad851b4582d
Jesse Suksia <djesu004@edu.xamk.fi>
2025-02-26 18:26:03 +0200

    Somelinkit footeriin

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import '../App.css';
-import { Box, Typography } from '@mui/material';
+import { Box, IconButton, Typography } from '@mui/material';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 const Footer: React.FC = (): React.ReactElement => {
 
@@ -10,6 +12,15 @@ const Footer: React.FC = (): React.ReactElement => {
         <Box className='App-footer'>
 
             <Typography variant='h6'>&copy; Jesse Suksia 2025</Typography>
+
+            <IconButton href='https://www.github.com/djesu004xamk' color='secondary'>
+                <GitHubIcon />
+            </IconButton>
+
+            <IconButton href='https://www.linkedin.com/in/jesse-suksia' color='primary'>
+                <LinkedInIcon />
+            </IconButton>
+
 
         </Box>
 
